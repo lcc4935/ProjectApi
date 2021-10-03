0b47c8e4aafa1e9e906c83a77099fbd4feaca937
Lauren <lcc4935@rit.edu>
2021-10-03 15:12:17 -0400

    copying over from api-assignment-ii for a starting point

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,64 +1,62 @@
-//function to send a json object
+const users = {};
+
 const respondJSON = (request, response, status, object) => {
-  //set status code and content type (application/json)
   response.writeHead(status, { 'Content-Type': 'application/json' });
-  //stringify the object (so it doesn't use references/pointers/etc)
-  //but is instead a flat string object.
-  //Then write it to the response.
   response.write(JSON.stringify(object));
-  //Send the response to the client
   response.end();
 };
 
-//function to show a success status code
-const success = (request, response) => {
-  //message to send
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getUsers = (request, response) => {
   const responseJSON = {
-    message: 'This is a successful response',
+    users,
   };
 
-  //send our json with a success status code
   respondJSON(request, response, 200, responseJSON);
 };
 
-//function to show a bad request without the correct parameters
-const badRequest = (request, response, params) => {
-  //message to send
+const addUser = (request, response, body) => {
   const responseJSON = {
-    message: 'This request has the required parameters',
+    message: 'Name and age are both required',
   };
 
-  //if the request does not contain a valid=true query parameter
-  if(!params.valid || params.valid !== 'true') {
-    //set our error message
-    responseJSON.message = 'Missing valid query parameter set to true';
-    //give the error a consistent id 
-    responseJSON.id = 'badRequest';
-    //return our json with a 400 bad request code
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
     return respondJSON(request, response, 400, responseJSON);
   }
 
-  //if the parameter is here, send json with a success status code
-  return respondJSON(request, response, 200, responseJSON);
+  let responseCode = 201;
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+  return respondJSONMeta(request, response, responseCode);
 };
 
-//function to show not found error
-const notFound = (request, response) => {
-  //error message with a description and consistent error id
+const notReal = (request, response) => {
   const responseJSON = {
-    message: 'The page you are looking for was not found.',
+    message: 'The page you are looking for was not found',
     id: 'notFound',
   };
 
-  //return our json with a 404 not found error code
-  respondJSON(request, response, 404, responseJSON);
+  const responseCode = 404;
+  return respondJSON(request, response, responseCode, responseJSON);
 };
 
-//exports to set functions to public.
-//In this syntax, you can do getIndex:getIndex, but if they
-//are the same name, you can short handle to just getIndex,
 module.exports = {
-  success,
-  badRequest,
-  notFound,
+  getUsers,
+  addUser,
+  notReal,
 };
--- src/server.js
+++ src/server.js
@@ -1,44 +1,56 @@
-const http = require('http'); //pull in the http server module
-const url = require('url'); //pull in the url module
-//pull in the query string module
-const query = require('querystring'); 
-//pull in our html response handler file
-const htmlHandler = require('./htmlResponses.js'); 
-//pull in our json response handler file
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
-//set the port. process.env.PORT and NODE_PORT are for servers like heroku
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//key:value object to look up URL routes to specific functions
-const urlStruct = {
-  '/': htmlHandler.getIndex,
-  '/success': jsonHandler.success,
-  '/badRequest': jsonHandler.badRequest,
-  notFound: jsonHandler.notFound,
-};
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const body = [];
 
-//handle HTTP requests. In node the HTTP server will automatically
-//send this function request and pre-filled response objects.
-const onRequest = (request, response) => {
-  //parse the url using the url module
-  //This will let us grab any section of the URL by name
-  const parsedUrl = url.parse(request.url);
-  
-  //grab the query parameters (?key=value&key2=value2&etc=etc)
-  //and parse them into a reusable object by field name
-  const params = query.parse(parsedUrl.query);
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
 
-  //check if the path name (the /name part of the url) matches 
-  //any in our url object. If so call that function. If not, default to index.
-  if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, params);
-  } else {
-    urlStruct.notFound(request, response, params);
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      jsonHandler.addUser(request, response, bodyParams);
+    });
   }
 };
 
-//start HTTP server 
+const handleGet = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/') {
+    htmlHandler.getIndex(request, response);
+  } else if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else {
+    jsonHandler.notReal(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
+};
+
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
